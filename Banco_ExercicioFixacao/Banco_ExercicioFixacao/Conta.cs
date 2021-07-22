7b4ce6a8421a53f5ba8ed140d05a86d9f15649db
Carlos Eduardo Santos Xavier <62305615+Cxavier14@users.noreply.github.com>
2021-07-22 15:55:57 -0300

    exercicioDeFixacao_banco

---
+++ Banco_ExercicioFixacao/Banco_ExercicioFixacao/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco_ExercicioFixacao {
+    class Conta {
+
+        // propriedades
+        public string Titular { get; set; }
+        public int NumConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        // construtores
+        public Conta(int numero, string titular) {
+            NumConta = numero;
+            Titular = titular;
+        }
+
+        public Conta(int numero, string titular, double saldo) : this(numero, titular) {
+            Saldo = saldo;
+        }
+
+        // metodos
+        public void DepositoConta(double deposito) {
+            Saldo += deposito;
+        }
+
+        public void SaqueConta(double saque) {
+            Saldo -=saque + 5.00;
+        }
+
+        public override string ToString() {
+            return $"Conta { NumConta}, Titular: {Titular}, " +
+                $" Saldo: {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Banco_ExercicioFixacao/Banco_ExercicioFixacao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Banco_ExercicioFixacao {
+    class Program {
+        static void Main(string[] args) {
+
+            Conta conta;
+
+            Console.Write("Entre com o numero da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial (s/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S') {
+                double depInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numConta, titular, depInicial);
+            }
+            else {
+                conta = new Conta(numConta, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.DepositoConta(dep);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre com um valor para saque: ");
+            double saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.SaqueConta(saq);
+            Console.WriteLine(conta);
+            
+
+
+        }
+    }
+}
